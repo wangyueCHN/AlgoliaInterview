7e85b2662965d316eb58415e8361f5a73c1e7df9
wangyueCHN <wangyue0204@outlook.com>
2023-12-18 23:13:00 +0100

    Add files via upload

---
+++ search.py
@@ -0,0 +1,32 @@
+from algoliasearch.search_client import SearchClient
+
+client = SearchClient.create('0ID4OD4XQ9', 'ea507a8fa26bbc05662d550aad4de774')
+index = client.init_index('restaurantINFO')
+
+
+index.set_settings({
+    "searchableAttributes": ["name", "address", "dining_style"],
+    "customRanking": ["desc(reviews_count)"],
+    'attributesForFaceting': [
+        'filterOnly(price_range)',
+        'filterOnly(stars_count)',
+        'filterOnly(food_type)',
+        'searchable(city)',
+        'searchable(country)'
+    ]
+})
+
+query = "restaurant"
+filters = "available = 1 AND (food_type:French) AND stars_count > 3.5"
+
+result_search = index.search('query', {
+    'filters': filters
+})
+result_facet = index.search_for_facet_values('city', 'New York')
+result_geo = index.search('query', {
+    'aroundLatLng': '48.84, -2.38'
+})
+
+print(result_search)
+print(result_facet)
+print(result_geo)
